7cc3924f9b223f03979c5b41018401aaf48844c3
s0p02bt <s0p02bt@walmart.com>
2020-03-29 19:36:08 +0530

    synchronous

--- src/main/java/com/logging/loggerLibrary/Main.java
+++ src/main/java/com/logging/loggerLibrary/Main.java
@@ -4,6 +4,10 @@ import com.logging.loggerLibrary.enums.Level;
 import com.logging.loggerLibrary.module.LoggerImpl;
 import com.logging.loggerLibrary.module.LoggerLib;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
 
@@ -19,17 +23,49 @@ public class Main {
 //        String dbPassword = System.getenv("db_password");
 
         String logLevel = Level.INFO.toString();
-        String infoSink = "DB";
+        String infoSink = "FILE";
         String errorSink = "FILE";
         String fatalSink = "FILE";
         String debugSink = "FILE";
-        String filePath = "testFile.txt";
+        String filePath = "testFile2.txt";
         String dbUrl = System.getenv("db_url");
 
-        LoggerLib logger = new LoggerImpl(Level.valueOf(logLevel), infoSink, errorSink, fatalSink, debugSink,
+        LoggerLib log = new LoggerImpl(Level.valueOf(logLevel), infoSink, errorSink, fatalSink, debugSink,
+                filePath, dbUrl);
+        LoggerLib log1 = new LoggerImpl(Level.valueOf(logLevel), infoSink, errorSink, fatalSink, debugSink,
+                filePath, dbUrl);
+        LoggerLib log2 = new LoggerImpl(Level.valueOf(logLevel), infoSink, errorSink, fatalSink, debugSink,
                 filePath, dbUrl);
 
-        logger.info("message", Main.class.getName());
+        Integer numberOfThreads = 10;
+        Runnable task = () -> {
+            for(int i = 0; i < 10; i++) {
+                log.info("test " + i, "1");
+            }
+        };
+        Runnable task1 = () -> {
+            for(int i = 0; i < 10; i++) {
+                log1.info("test " + i, "1");
+            }
+        };
+        Runnable task2 = () -> {
+            for(int i = 0; i < 10; i++) {
+                log1.info("test " + i, "1");
+            }
+        };
+
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        for(int i = 0; i < numberOfThreads; i++) {
+            if(i%3 == 0)
+                executor.execute(task);
+            else if(i%3 == 1)
+                executor.execute(task1);
+            else
+                executor.execute(task2);
+        }
+
+        executor.shutdown();
+        //log.info("message", Main.class.getName());
 
         System.out.println("message written");
     }
--- src/main/java/com/logging/loggerLibrary/sink/FileSink.java
+++ src/main/java/com/logging/loggerLibrary/sink/FileSink.java
@@ -2,21 +2,21 @@ package com.logging.loggerLibrary.sink;
 
 import com.logging.loggerLibrary.model.Message;
 
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.*;
 
 public class FileSink implements Sink {
 
     @Override
     public void writeInSink(Message message, String filePath) {
 
-        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream(filePath), "utf-8"))) {
-            writer.write(message.getTimestamp() + "  [" + message.getLevel() + "] " + message.getNamespace() +
-                    " -  " + message.getContent());
-        } catch (Exception e) {
+        try
+        {
+            FileWriter fw = new FileWriter(filePath, true);
+            fw.write(message.getTimestamp() + "  [" + message.getLevel() + "] " + message.getNamespace() +
+                    " -  " + message.getContent() + "\n");
+            fw.close();
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
     }
